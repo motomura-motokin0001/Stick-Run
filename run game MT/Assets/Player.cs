d6e228850230cf723b2e6fbf1ff2acd20daefbbd
本村煌雅 <human.kagoshima.kouga@gmail.com>
2024-05-15 16:04:53 +0900

    シーンの追加

--- run game MT/Assets/Player.cs
+++ run game MT/Assets/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     public float jumpP = 300f; // ジャンプ力
 
     Rigidbody2D rbody; // リジッドボディを使うための宣言
-    
+
     private Vector3 afterRectVelocity;
 
     // Start is called before the first frame update
@@ -16,7 +16,7 @@ public class Player : MonoBehaviour
     {
         // リジッドボディ2Dをコンポーネントから取得して変数に入れる
         rbody = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
@@ -25,10 +25,10 @@ public class Player : MonoBehaviour
         // ジャンプをするためのコード（もしスペースキーが押されて、上方向に速度がない時に）
         if (Input.GetKeyDown(KeyCode.Space) && rbody.velocity.y == 0)
         {
-             // リジッドボディに力を加える（上方向にジャンプ力をかける）
+            // リジッドボディに力を加える（上方向にジャンプ力をかける）
             rbody.AddForce(transform.up * jumpP);
         }
-        
+
     }
 
     private void FixedUpdate()
@@ -36,5 +36,5 @@ public class Player : MonoBehaviour
         //リジッドボディに一定の速度を入れる（横移動の速度, リジッドボディのyの速度）
         rbody.velocity = new Vector2(speed, rbody.velocity.y);
     }
-    
+
 }
---
+++ run game MT/Assets/pause.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class PauseScript : MonoBehaviour {
+ 
+	[SerializeField]
+	//　ポーズした時に表示するUIのプレハブ
+	private GameObject pauseUIPrefab;
+	//　ポーズUIのインスタンス
+	private GameObject pauseUIInstance;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("p")) {
+			if (pauseUIInstance == null) {
+				pauseUIInstance = GameObject.Instantiate (pauseUIPrefab) as GameObject;
+				Time.timeScale = 0f;
+			} else {
+				Destroy (pauseUIInstance);
+				Time.timeScale = 1f;
+			}
+		}
+	}
+}
